ab9bb02f661c4d9544c7f4ea2046d782e62d8455
github username <github email address>
2020-04-28 01:53:55 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,129 +1,108 @@
-let addToy = false;
-
-
-function createToyCard(toy) {
-    const toyCollection = document.getElementById("toy-collection")
-    const div = document.createElement('div')
-    const h2 = document.createElement('h2')
-    const img = document.createElement('img')
-    const p = document.createElement('p')
-    const likeButton = document.createElement('button')
-
-    likeButton.className = 'like-btn'
-    likeButton.innerText = 'Like <3'
-    img.setAttribute('src', toy.image)
-    p.innerText = toy.likes
-    img.className = 'toy-avatar'
-    h2.innerText = toy.name
-    div.className = 'card'
-
-    div.appendChild(h2)
-    div.appendChild(img)
-    div.appendChild(p)
-    div.appendChild(likeButton)
-
-    toyCollection.appendChild(div)
-
-    const configLike = {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      },
-      body: JSON.stringify({
-        likes: toy.likes + 1
-      })
-    }
-    likeButton.addEventListener('click', function(event) {
-      event.preventDefault();
-      fetch(`http://localhost:3000/toys/${toy.id}`, configLike)
-      .then(function(resp) {
-        return resp.json()
-      })
-      .then(function(toy) {
-        const toyCollection = document.getElementById("toy-collection")
-        const newDiv = document.createElement('div')
-        const newH2 = document.createElement('h2')
-        const newImg = document.createElement('img')
-        const newP = document.createElement('p')
-        const likeButton = document.createElement('button')
-    
-        likeButton.className = 'like-btn'
-        likeButton.innerText = 'Like <3'
-        newImg.setAttribute('src', toy.image)
-        newP.innerText = toy.likes
-        newImg.className = 'toy-avatar'
-        newH2.innerText = toy.name
-        newDiv.className = 'card'
-    
-        newDiv.appendChild(newH2)
-        newDiv.appendChild(newImg)
-        newDiv.appendChild(newP)
-        newDiv.appendChild(likeButton)
-
-        toyCollection.replaceChild(newDiv,div)
-      })
-    })
-    // Why can I not click the like button more than once?
-}
 
 document.addEventListener("DOMContentLoaded", () => {
+  let addToy = false;
   const addBtn = document.querySelector("#new-toy-btn");
-  const toyForm = document.querySelector(".container");
-  fetch("http://localhost:3000/toys")
-  .then(function(resp) {
-    return resp.json()
-  })
-  .then(function(data) {
-    // console.log(data)
-    data.forEach(function(toy) {
-      createToyCard(toy);
-    })
-  })
-  
-  toyForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    // console.log(event.target.name.value)
-    // console.log(event.target.image.value)
-
-    const configToy = {
-      // method
-      method: 'POST',
-      //header
-      headers: {
-        "Content-Type": 'application/json',
-        Accept: 'application/json'
-      },
-      //body
-      body: JSON.stringify({
-        name: event.target.name.value,
-        image: event.target.image.value
-      })
-    }
-    
-    fetch("http://localhost:3000/toys", configToy)
-    .then(function(resp) {
-      return resp.json()
-    })
-    .then(function(toy) {
-      console.log(toy)
-      if (toy.id) {
-        createToyCard(toy)
-      } else {
-        console.log("error")
-      }
-    })
-  })
-
-
+  const toyFormContainer = document.querySelector(".container");
 
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
-      toyForm.style.display = "block";
+      toyFormContainer.style.display = "block";
+      toyFormContainer.addEventListener('submit', function(e) {
+        e.preventDefault();
+        postToy(e)
+      })
     } else {
-      toyForm.style.display = "none";
+      toyFormContainer.style.display = "none";
     }
   });
+  fetchToys()
 });
+
+function fetchToys() {
+  fetch(`http://localhost:3000/toys`)
+  .then(resp=>resp.json())
+  .then(toys=>{
+    toys.forEach(toy=>{
+      createToyCard(toy)
+    })
+  })
+}
+
+function fetchToy(toy) {
+  fetch(`http://localhost:3000/toys/${toy.id}`)
+  .then(resp=>resp.json())
+  .then(toy=>console.log(toy))
+}
+
+function postToy(event) {
+  console.log(event.target.image.value)
+  toy = {
+    name: event.target.name.value,
+    image: event.target.image.value,
+    likes: 0
+  }
+  fetch(`http://localhost:3000/toys`, {
+    method: 'POST',
+    headers: {
+      'Content-Type':'Application/json',
+      Accepts: 'Application/json'
+    },
+    body: JSON.stringify(toy)
+  })
+  .then(resp=>resp.json())
+  .then(toy=>{
+    const toyFormContainer = document.querySelector(".container");
+    createToyCard(toy)
+  })
+}
+
+function deleteToy(toy, div) {
+  fetch(`http://localhost:3000/toys/${toy.id}`, {
+    method: 'DELETE'
+  })
+  .then(resp=>resp.json())
+  .then(div.remove())
+}
+
+function addLike(toy, p) {
+  fetch(`http://localhost:3000/toys/${toy.id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type':'Application/json',
+      Accepts: 'Application/json'
+    },
+    body: JSON.stringify({likes: toy.likes + 1})
+  })
+  .then(resp=>resp.json())
+  .then(p.innerText = parseInt(toy.likes += 1))
+}
+
+function createToyCard(toy) {
+  const toyCollection = document.querySelector('#toy-collection')
+  const div = document.createElement('div')
+  const h2 = document.createElement('h2')
+  const img = document.createElement('img')
+  const p = document.createElement('p')
+  const likebtn = document.createElement('button')
+  const deletebtn = document.createElement('button')
+
+  div.className = 'card'
+  h2.innerText = toy.name
+  img.src = toy.image
+  img.className = 'toy-avatar'
+  p.innerText = toy.likes
+  likebtn.className = 'like-btn'
+  likebtn.innerText = 'Like'
+  deletebtn.className = 'like-btn'
+  deletebtn.innerText = 'Delete'
+
+  deletebtn.addEventListener('click', () => deleteToy(toy, div))
+  likebtn.addEventListener('click', () => addLike(toy, p))
+
+  div.append(h2, img, p, likebtn, deletebtn)
+  toyCollection.appendChild(div)
+}
+
+
